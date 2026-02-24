b96f5af2781a9ba511c14a63866cc7d9f3db578c
rattus-aristarchus <lankin.m.a@gmail.com>
2026-02-24 05:38:25 +0300

    changes after text

--- allurerc.js
+++ allurerc.js
@@ -1,22 +1,48 @@
 export default {
+  // Общий заголовок
   name: "Демонстрация деревьев в Allure 3",
+  // Путь к папке с отчётом
   output: "./allure-report",
+  // Путь к файлу с историей
   historyPath: "./test-history/history.jsonl",
+  // Плагины
   plugins: {
+    // Плагин для группировки по фичам
     "awesome-by-feature": {
       import: "@allurereport/plugin-awesome",
       options: {
+        // Имя отчёта
         reportName: "Группируем по фичам",
+        // Язык отчёта
         reportLanguage: "ru",
+        // Поле, по которому группируются тесты в отчёте
         groupBy: ["epic", "feature", "story"],
       },
     },
+    // Плагин для группировки по сюитам
     "awesome-by-suite": {
       import: "@allurereport/plugin-awesome",
       options: {
         reportName: "Группируем по сюитам",
         reportLanguage: "ru",
         groupBy: ["suite"],
+      },
+    },
+    // Плагин для классического отображения
+    "classic view": {
+      import: "@allurereport/plugin-classic",
+      options: {
+        reportName: "Классическое отображение",
+        reportLanguage: "ru",
+      },
+    },
+    // Плагин для отображения тестов по уровням
+    "layer view": {
+      import: "@allurereport/plugin-awesome",
+      options: {
+        reportName: "Группируем по уровням",
+        reportLanguage: "ru",
+        groupBy: ["level"],
       },
     },
   },
--- test/trees_test.py
+++ test/trees_test.py
@@ -1,21 +1,35 @@
 import allure
 
+# Сортировка по фичам:
 @allure.feature("Feature 1")
+# Сортировка по сюитам:
 @allure.suite("Suite 1")
 @allure.title("Первый тест")
-def test_allure_3_first():
-    pass
-
-
-@allure.feature("Feature 1")
-@allure.suite("Suite 2")
-@allure.title("Второй тест")
-def test_allure_3_second():
+def test_first():
     pass
 
 
 @allure.feature("Feature 2")
 @allure.suite("Suite 2")
+@allure.title("Второй тест")
+def test_second():
+    pass
+
+
+@allure.feature("Feature 1")
+@allure.suite("Suite 2")
 @allure.title("Третий тест")
-def test_allure_3_third():
+def test_third():
+    pass
+
+
+@allure.label("level", "UI")
+@allure.title("Тест на UI")
+def test_ui():
+    pass
+
+
+@allure.label("level", "API")
+@allure.title("Тест на API")
+def test_api():
     pass
